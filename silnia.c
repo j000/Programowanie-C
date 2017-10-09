8acbb2c0bff5acee6c1897a700299fbda7490aef
j000 <jarymut+github@gmail.com>
2017-10-10 00:31:52 +0200

    Dodaj program Silnia

---
+++ silnia.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned long int uli;
+
+uli silnia(int n) {
+	uli s=1;
+	int i;
+	for (i=1; i<=n; ++i) {
+		s*=i;
+	}
+	return s;
+}
+
+int main() {
+	int n;
+	printf("Podaj liczbe: ");
+	scanf("%d", &n);
+	printf("%d! = %lu\n", n, silnia(n));
+	return EXIT_SUCCESS;
+}
