4210c9430e588e81460bba75b2d4c4ebcb155a4c
j000 <jarymut+github@gmail.com>
2017-10-19 07:34:17 +0200

    Dodaj sprawdzenie przepełnienia

--- silnia.c
+++ silnia.c
@@ -6,23 +6,38 @@ typedef unsigned long int uli;
 /**
  * liczy slinie liczby
  */
-uli silnia(int);
+/*uli silnia(int);*/
+uli silnia(int,char*);
 
 
-uli silnia(int n) {
+uli silnia(int n, char *error) {
 	uli s=1;
+	uli t=1;
 	int i;
-	for (i=1; i<=n; ++i) {
-		s*=i;
+	for (i=2; i<=n; ++i) {
+		t*=i;
+		if (t>s) {
+			s=t;
+		} else {
+			*error = 1;
+			return s;
+		}
 	}
 	return s;
 }
 
 int main(int argc, char *argv[]) {
 	int n;
+	char e=0;
+	uli tmp;
 	printf("Podaj liczbe: ");
 	scanf("%d", &n);
-	printf("%d! = %lu\n", n, silnia(n));
+	tmp = silnia(n, &e);
+	if (e) {
+		printf("Przepełnienie!\nNie mogę policzyć %d!.\nWynik: %lu\n", n, tmp);
+		return EXIT_FAILURE;
+	}
+	printf("%d! = %lu\n", n, tmp);
 	return EXIT_SUCCESS;
 }
 
