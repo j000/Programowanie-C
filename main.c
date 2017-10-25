212429de95df6e08bc650fe8af41cc35713dfcf0
j000 <jarymut+github@gmail.com>
2017-10-25 08:55:03 +0200

    Popraw wypisywanie liczb

--- main.c
+++ main.c
@@ -7,6 +7,10 @@ int main(int argc, char** argv) {
 	double a,b,c,x1,x2;
 	int n;
 	/* **** */
+	#ifndef __STDC_IEC_559__
+	puts("Warning: __STDC_IEC_559__ not defined. IEEE 754 floating point not fully supported.");
+	#endif
+	/* **** */
 	printf("Równanie kwadratowe a*x^2 + b*x + c\n");
 	printf("Podaj a: ");
 	scanf("%lf", &a);
@@ -16,13 +20,13 @@ int main(int argc, char** argv) {
 	scanf("%lf", &c);
 	/* **** */
 	n = rownanieKwadratowe(a, b, c, &x1, &x2);
-	printf("%f * x^2 + %f * x + %f = ", a, b, c);
+	printf("\n%.2f * x^2 + %.2f * x + %.2f = ", a, b, c);
 	if (n == 0) {
 		printf("Brak rozwiązań\n");
 	} else if (n == 1) {
-		printf("%f\n", x1);
+		printf("%.2f\n", x1);
 	} else {
-		printf("%f i %f\n", x1, x2);
+		printf("%.2f i %.2f\n", x1, x2);
 	}
 	return EXIT_SUCCESS;
 }
