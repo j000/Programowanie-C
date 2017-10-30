f943e44adafbd62a79b2fc8ec1d743246cce8870
j000 <jarymut+github@gmail.com>
2017-10-30 13:56:10 +0100

    Przenieś kod z main.c do rkw.c

--- main.c
+++ main.c
@@ -1,34 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int rownanieKwadratowe(double, double, double, double*, double*);
+int kwadratowe(int, char**);
 
 int main(int argc, char** argv) {
-	double a,b,c,x1,x2;
-	int n;
-	/* **** */
-	#ifndef __STDC_IEC_559__
-	puts("Warning: __STDC_IEC_559__ not defined. IEEE 754 floating point not fully supported.");
-	#endif
-	/* **** */
-	printf("Równanie kwadratowe a*x^2 + b*x + c\n");
-	printf("Podaj a: ");
-	scanf("%lf", &a);
-	printf("Podaj b: ");
-	scanf("%lf", &b);
-	printf("Podaj c: ");
-	scanf("%lf", &c);
-	/* **** */
-	n = rownanieKwadratowe(a, b, c, &x1, &x2);
-	printf("\n%.2f * x^2 + %.2f * x + %.2f = ", a, b, c);
-	if (n == 0) {
-		printf("Brak rozwiązań\n");
-	} else if (n == 1) {
-		printf("%.2f\n", x1);
-	} else {
-		printf("%.2f i %.2f\n", x1, x2);
-	}
-	return EXIT_SUCCESS;
+	return kwadratowe(argc, argv);
 }
 
 /* vim: tabstop=4 shiftwidth=0 noexpandtab
--- rkw.c
+++ rkw.c
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int rownanieKwadratowe(double a, double b, double c, double* x1, double* x2) {
 	double delta = b * b - 4 * a * c;
@@ -15,6 +17,30 @@ int rownanieKwadratowe(double a, double b, double c, double* x1, double* x2) {
 	*x1 = (-b - delta) / a / 2;
 	*x2 = (-b + delta) / a / 2;
 	return 2;
+}
+
+int kwadratowe(int argc, char* argv[]) {
+	double a,b,c,x1,x2;
+	int n;
+	/* **** */
+	printf("Równanie kwadratowe a*x^2 + b*x + c\n");
+	printf("Podaj a: ");
+	scanf("%lf", &a);
+	printf("Podaj b: ");
+	scanf("%lf", &b);
+	printf("Podaj c: ");
+	scanf("%lf", &c);
+	/* **** */
+	n = rownanieKwadratowe(a, b, c, &x1, &x2);
+	printf("\n%.2f * x^2 + %.2f * x + %.2f = ", a, b, c);
+	if (n == 0) {
+		printf("Brak rozwiązań\n");
+	} else if (n == 1) {
+		printf("%.2f\n", x1);
+	} else {
+		printf("%.2f i %.2f\n", x1, x2);
+	}
+	return EXIT_SUCCESS;
 }
 
 /* vim: tabstop=4 shiftwidth=0 noexpandtab
